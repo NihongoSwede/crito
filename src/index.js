46721eb938905234d6599b172c6c55644336d104
mathias Hedenborg <mathias.hedenborg@learnet.se>
2023-10-25 18:56:23 +0200

    vilka ändringar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Home from './views/Home';
+import Contacts from './views/Contacts';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './Index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Contacts' element={<Contacts/>} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='p-20 bg-red-500'>
+      Hej
+    </div>
+  )
+}
+
+export default Home
+
