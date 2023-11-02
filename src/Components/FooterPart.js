c9353f4e570868ff73ecc343a5f075fe31664c0f
NihongoSwede <mhedenborg18@gmail.com>
2023-11-02 20:11:30 +0100

    vilka ändringar

--- src/Components/FooterPart.js
+++ src/Components/FooterPart.js
@@ -1,55 +1,46 @@
 import React from 'react';
 import bottomlogo from '../Assets/Logo (1).svg';
 
-const FooterPart = () => {
-  return (
-    <section className="bottom-section1">
-      <div className="container">
-        <div className="picture-text-div">
-          <img src={bottomlogo} alt="" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-            obcaecati voluptas voluptates! Voluptates laborum nam ratione minus
-            necessitatibus, iure praesentium.
-          </p>
-        </div>
-
-        <div className="rightPartsec">
-          <div className="divStack">
-            <h4>Company</h4>
-            <a href="">About</a>
-            <a href="">Features</a>
-            <a href="">Work</a>
-            <a href="">Career</a>
-          </div>
-
-          <div className="divStack">
-            <h4>Help</h4>
-            <a href="">Customer Support</a>
-            <a href="">Delivery Details</a>
-            <a href="">Terms & Conditions</a>
-            <a href="">Privacy & Policy</a>
-          </div>
-
-          <div className="divStack">
-            <h4>Resources</h4>
-            <a href="">Free eBooks</a>
-            <a href="">Development Tutorial</a>
-            <a href="">How to -Blog</a>
-            <a href="">Youtube Playlist</a>
-          </div>
-
-          <div className="divStack">
-            <h4>Link</h4>
-            <a href="">Free eBooks</a>
-            <a href="">Development Tutorial</a>
-            <a href="">How to -Blog</a>
-            <a href="">Youtube Playlist</a>
-          </div>
-        </div>
+const FooterPart = () => (
+  <section className="bottom-section1">
+    <div className="container">
+      <div className="picture-text-div">
+        <img src={bottomlogo} alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          obcaecati voluptas voluptates! Voluptates laborum nam ratione minus
+          necessitatibus, iure praesentium.
+        </p>
       </div>
-    </section>
-  );
-};
 
+      <div className="rightPartsec">
+        {[
+          {
+            title: 'Company',
+            links: ['About', 'Features', 'Work', 'Career'],
+          },
+          {
+            title: 'Help',
+            links: ['Customer Support', 'Delivery Details', 'Terms & Conditions', 'Privacy & Policy'],
+          },
+          {
+            title: 'Resources',
+            links: ['Free eBooks', 'Development Tutorial', 'How to -Blog', 'Youtube Playlist'],
+          },
+          {
+            title: 'Link',
+            links: ['Free eBooks', 'Development Tutorial', 'How to -Blog', 'Youtube Playlist'],
+          },
+        ].map((section, index) => (
+          <div className="divStack" key={index}>
+            <h4>{section.title}</h4>
+            {section.links.map((link, linkIndex) => (
+              <a key={linkIndex} href="">{link}</a>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
 export default FooterPart;
